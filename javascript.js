0b8fd4287dff24aba9591bbea452be35882c3acd
korybiggerstaff <biggerstaff.k@gmail.com>
2024-02-22 16:56:00 -0500

    refactor to avoid unnecessary conversion to lowercase

--- javascript.js
+++ javascript.js
@@ -1,30 +1,29 @@
 function playRound(playerSelection, computerSelection) {
     let player = playerSelection.toLowerCase() //convert player input to lowercase
-    let computer = computerSelection.toLowerCase() //convert computer choice to lowercase
     console.log("You picked " + player);
-    console.log("The computer picked " + computer);
+    console.log("The computer picked " + computerSelection);
     if (player = "rock") {
-        if (computer = "rock") {
+        if (computerSelection = "rock") {
             return "It's a tie!";
-        } else if (computer = "scissors") {
+        } else if (computerSelection = "scissors") {
             return "You win! Rock beats scissors.";
-        } else if (computer = "paper") {
+        } else if (computerSelection = "paper") {
             return "You lose! Paper beats rock.";
         }
     } else if (player = "paper") {
-        if (computer = "rock") {
+        if (computerSelection = "rock") {
             return "You win! Paper beats rock.";
-        } else if (computer = "scissors") {
+        } else if (computerSelection = "scissors") {
             return "You lose! Scissors beats paper";
-        } else if (computer = "paper") {
+        } else if (computerSelection = "paper") {
             return "It's a tie!";
         }
     } else if (player = "scissors") {
-        if (computer = "rock") {
+        if (computerSelection = "rock") {
             return "You lose! Rock beats scissors";
-        } else if (computer = "scissors") {
+        } else if (computerSelection = "scissors") {
             return "It's a tie!";
-        } else if (computer = "paper") {
+        } else if (computerSelection = "paper") {
             return "You win! Scissors beats paper.";
         }
     }
@@ -34,13 +33,13 @@ function getComputerChoice() {
     let x = 0
     x = Math.floor(Math.random() * 3) + 1;
     if (x == 1) {
-        return "Rock";
+        return "rock";
     } else if (x == 2) {
-        return "Paper";
+        return "paper";
     } else if (x == 3) {
-        return "Scissors";
+        return "scissors";
     } else {return "Oops!"}
 }
-const playerSelection = "rock";
+const playerSelection = "Scissors";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
