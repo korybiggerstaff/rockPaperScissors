c60d81e519e4520523d3b073685c7ec19da94f8e
korybiggerstaff <biggerstaff.k@gmail.com>
2024-02-22 16:08:19 -0500

    add getComputerChoice() function

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let x = 0
+    x = Math.floor(Math.random() * 3) + 1;
+    if (x == 1) {
+        return "Rock";
+    } else if (x == 2) {
+        return "Paper";
+    } else if (x == 3) {
+        return "Scissors";
+    } else {return "Oops!"}
+}
